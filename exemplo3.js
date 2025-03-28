2147075afad05c300a308c6fea3c0e017801c527
mawavial <panzerkovic@gmail.com>
2025-03-28 14:32:52 -0300

    addd exemplo5 bulbasaur issue

--- exemplo3.js
+++ exemplo3.js
@@ -1,7 +1,11 @@
 const fetchPromise = fetch("https://pokeapi.co/api/v2/pokemon/pikachu"); 
  
 fetchPromise.then((response) => { 
-  console.log("Resposta recebida:", response.status); 
+  console.log("Resposta recebida:", response); 
 }); 
  
 console.log("Requisição iniciada…")
+
+//pending
+//fulfilled/resolved
+//rejected
--- exemplo5.js
+++ exemplo5.js
@@ -1,6 +1,8 @@
+new Promise ((resolve, reject) => {})
 const fetch1 = fetch("https://pokeapi.co/api/v2/pokemon/pikachu"); 
 const fetch2 = fetch("https://pokeapi.co/api/v2/pokemon/charizard"); 
-const fetch3 = fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur"); 
+const fetch3 = await fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur"); 
+ console.log(await fetch3.json());
  
 Promise.all([fetch1, fetch2, fetch3]) 
   .then((responses) => { 
@@ -10,3 +12,6 @@ Promise.all([fetch1, fetch2, fetch3])
     pokemons.forEach((pokemon) => console.log("Pokémon:", pokemon.name)); 
   }) 
   .catch((error) => console.error("Erro em uma das requisições", error)); 
+//pending
+//resolved
+//rejected
