2535e5289617bce1f29d7e30684d70a7c00cb77f
mawavial <panzerkovic@gmail.com>
2025-05-23 14:06:37 -0300

    commitei Thiago

---
+++ src/ex1.js
@@ -0,0 +1,5 @@
+var btn = document.getElementById('acaoBtn');
+var resultado = document.getElementById('resultado');
+btn.addEventListener('click', function () {
+    resultado.textContent = 'BOOM! 🎉';
+});
