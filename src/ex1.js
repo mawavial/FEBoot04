bd290e3e53f8bbdb77a955334f7b43d90856e669
mawavial <panzerkovic@gmail.com>
2025-05-29 14:12:51 -0300

    exorcicio dificil

--- src/ex1.js
+++
@@ -1,5 +0,0 @@
-var btn = document.getElementById('acaoBtn');
-var resultado = document.getElementById('resultado');
-btn.addEventListener('click', function () {
-    resultado.textContent = 'BOOM! 🎉';
-});
